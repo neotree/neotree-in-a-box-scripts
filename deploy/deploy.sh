d23ca4e9a3929830f7aac058086adcf5be295ca4
mbaradza <mbaradza1@gmail.com>
2026-04-20 15:54:42 +0200

    Fix Logs

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,7 +11,6 @@ NEOTREE_RUN_LOG="$NEOTREE_LOG_DIR/deploy_$(date +%Y%m%d_%H%M%S).log"
 exec > >(tee -a "$NEOTREE_RUN_LOG") 2>&1
 trap 'log_error "Deployment failed at line $LINENO. See $NEOTREE_RUN_LOG"; exit 1' ERR
 
-log_info "Starting Neotree Story 6 deployment"
 log_info "Deploy log: $NEOTREE_RUN_LOG"
 
 bash "$NEOTREE_BASE_DIR/node-api/deploy.sh"
